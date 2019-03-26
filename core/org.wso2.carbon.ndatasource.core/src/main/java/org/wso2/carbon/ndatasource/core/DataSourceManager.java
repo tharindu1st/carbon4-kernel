f88ca8dfef6b16fba7dd0c0eecc04e6aab9e7e5b
Tharindu Dharmarathna <tharindua@wso2.com>
2019-03-26 17:31:48 +0530

    Implement Secret Section

--- core/org.wso2.carbon.ndatasource.core/src/main/java/org/wso2/carbon/ndatasource/core/DataSourceManager.java
+++ core/org.wso2.carbon.ndatasource.core/src/main/java/org/wso2/carbon/ndatasource/core/DataSourceManager.java
@@ -15,9 +15,9 @@
  */
 package org.wso2.carbon.ndatasource.core;
 
+import org.apache.axiom.om.OMElement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.w3c.dom.Document;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.ndatasource.common.DataSourceConstants;
 import org.wso2.carbon.ndatasource.common.DataSourceException;
@@ -26,7 +26,6 @@ import org.wso2.carbon.ndatasource.core.internal.DataSourceServiceComponent;
 import org.wso2.carbon.ndatasource.core.utils.DataSourceUtils;
 import org.wso2.carbon.utils.CarbonUtils;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
-import org.wso2.securevault.SecretResolverFactory;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -175,10 +174,10 @@ public class DataSourceManager {
 
         try {
             JAXBContext ctx = JAXBContext.newInstance(SystemDataSourcesConfiguration.class);
-            Document doc = DataSourceUtils.convertToDocument(sysDSFile);
+            OMElement doc = DataSourceUtils.convertToDocument(sysDSFile);
             DataSourceUtils.secureResolveDocument(doc, true);
             SystemDataSourcesConfiguration sysDS = (SystemDataSourcesConfiguration) ctx.createUnmarshaller().
-                    unmarshal(doc);
+                    unmarshal(doc.getXMLStreamReader());
             this.addDataSourceProviders(sysDS.getProviders());
             DataSourceRepository dsRepo = this.getDataSourceRepository(
                     MultitenantConstants.SUPER_TENANT_ID);
